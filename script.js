2f55c5e11d72538388d6f8bf883376e66a48f012
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-01-19 09:32:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var email = document.querySelector('#mail')
+var password = document.querySelector('#pass')
 
 function validar(){
     if (email.value.length == 0){
         window.alert('Preencha o campo de e-mail');
+        document.querySelector('#mail').focus();
     }
+    else if (email.value.indexOf('@') == -1 
+    || email.value.indexOf('.') == -1 ){
+        window.alert("Insira um e-mail válido!");
+        document.querySelector('#mail').focus();
+     }
 }
