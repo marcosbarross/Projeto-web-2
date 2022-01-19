9dfef858ce046b4e5a5cf392ba984723c0f40e1e
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-01-19 09:50:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var email = document.querySelector('#mail')
 var password = document.querySelector('#pass')
+var cnpj = document.querySelector('#cnpj')
 
 function validar(){
     if (email.value.length == 0){
@@ -11,4 +12,12 @@ function validar(){
         window.alert("Insira um e-mail válido!");
         document.querySelector('#mail').focus();
      }
+}
+function validarcnpj(){
+    if (cnpj.value.length == 0){
+        window.alert("Preencha o campo do CNPJ!");
+    }
+    else if (cnpj.value.length != "14"){
+        window.alert("Número de CNPJ inválido!");
+    }
 }
