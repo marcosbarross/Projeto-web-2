1689ccf5db8ad5e688738e69cc04c180d9c3a5f1
Marcos Barros <81267060+marcosbarross@users.noreply.github.com>
2022-01-19 23:07:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ var email = document.querySelector('#mail')
 var password = document.querySelector('#pass')
 var cnpj = document.querySelector('#cnpj')
 
+//link não está funcionando no github
 function validar(){
+    if (email.value.length == 0){
+        window.alert('Preencha o campo de e-mail');
+        document.querySelector('#mail').focus();
+    }
+    else if (email.value.indexOf('@') == -1 
+    || email.value.indexOf('.') == -1 ){
+        window.alert("Insira um e-mail válido!");
+        document.querySelector('#mail').focus();
+     }
+     else {
+        window.open('/logged-pages/pagina-paciente.html')
+     }
+}
+
+//precisa trocar o link
+function validarloginmedico(){
     if (email.value.length == 0){
         window.alert('Preencha o campo de e-mail');
         document.querySelector('#mail').focus();
